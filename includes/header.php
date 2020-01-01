373fd5225d9b8bec966541e88836cac698434678
vikku088 <viveksharma490@gmail.com>
2020-01-02 01:03:50 +0530

    vk_muscic COde

---
+++ includes/header.php
@@ -0,0 +1,44 @@
+<?php
+include("includes/config.php");
+include("includes/classes/User.php");
+include("includes/classes/Artist.php");
+include("includes/classes/Album.php");
+include("includes/classes/Song.php");
+include("includes/classes/Playlist.php");
+
+//session_destroy(); LOGOUT
+
+if(isset($_SESSION['userLoggedIn'])) {
+	$userLoggedIn = new User($con, $_SESSION['userLoggedIn']);
+	$username = $userLoggedIn->getUsername();
+	echo "<script>userLoggedIn = '$username';</script>";
+}
+else {
+	header("Location: register.php");
+}
+
+?>
+
+<html>
+<head>
+	<title>Welcome to VKMusic!</title>
+	<link rel="icon" href="assets/images/logo.png" type="image/png">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="assets/js/script.js"></script>
+</head>
+
+<body>
+<!-- Loader Tag -->
+<div id="loader"></div>
+	<div id="mainContainer">
+
+		<div id="topContainer">
+
+			<?php include("includes/navBarContainer.php"); ?>
+
+			<div id="mainViewContainer">
+
+				<div id="mainContent">
